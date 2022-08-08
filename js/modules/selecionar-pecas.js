933bbf8e9f8f373c9a77a53a65c369c96ebcaf7f
Márcio Brust <brustmw21@gmail.com>
2022-08-08 16:33:54 -0300

    xadrez

--- js/modules/selecionar-pecas.js
+++ js/modules/selecionar-pecas.js
@@ -2,14 +2,14 @@ export default function initiSelecionarPecas() {
   const pontoPeca = Array.from(document.querySelectorAll("div"));
   const eventos = ["click", "tochstart"];
 
+  function pontoDoClick(events) {
+    if (events.target.className === "peaoBranco") {
+      console.log(this);
+    }
+  }
   pontoPeca.forEach((item) => {
     eventos.forEach((click) => {
       item.addEventListener(click, pontoDoClick);
     });
   });
-  function pontoDoClick(events) {
-    function loopCelula() {}
-    if (events.target.className === "peaoBranco") {
-    }
-  }
 }
