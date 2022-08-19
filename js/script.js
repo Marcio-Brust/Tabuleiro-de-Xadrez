0081df2e1cd6bebddb13165256a3291a00c8c006
Márcio Brust <brustmw21@gmail.com>
2022-08-19 15:37:23 -0300

    movimento bispo finalizado

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@ import initiAddClass from "./modules/add-class.js";
 import initiMovimentoPeaoBranco from "./modules/movimento-peao-branco.js";
 import initiMovimentoPeaoPreto from "./modules/movimento-peao-preto.js";
 import initiMovimentoGeral from "./modules/movimento-geral.js";
-import initiMovimentoBispoBranco from "./modules/movimento-bispo-branco.js";
-import initiMovimentoBispoBranco2 from "./modules/movimento-bispo-branco2.js";
+import initiMovimentoBispo from "./modules/movimento-bispo.js";
+import initiMovimentoBispo2 from "./modules/movimento-bispo2.js";
 initiAddClass();
 initiMovimentoPeaoBranco();
 initiMovimentoPeaoPreto();
 initiMovimentoGeral();
-initiMovimentoBispoBranco();
-initiMovimentoBispoBranco2();
+initiMovimentoBispo();
+initiMovimentoBispo2();
 /* Quando o span tiver a classe ativo a div irar fazer um loop com for e serar adiconado a classe ativo de acordo com a peça no span */
