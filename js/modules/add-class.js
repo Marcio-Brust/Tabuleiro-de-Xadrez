a36322f9a696afb2a79a40b5bb1f1543c5a9636e
Marcio-Brust <brustmw21@gmail.com>
2022-08-20 16:23:21 -0300

    mudanca na add-class

--- js/modules/add-class.js
+++ js/modules/add-class.js
@@ -5,22 +5,30 @@ export default function initiAddClass() {
     this.addClass = function (classe) {
       elemento.forEach((item) => {
         item.addEventListener("click", () => {
-          for (let i = 0; i < elemento.length; i++) {
-            elemento[i].classList.remove(classe);
-            elemento[i].classList.remove("movimento");
-          }
-          if (item.innerHTML !== "") {
-            const tabuleiro = document.querySelector(".tabuleiro");
-            if (tabuleiro.classList.contains("movimentoBranco")) {
-              if (item.classList.contains("branco")) {
-                item.classList.add(classe);
-                item.classList.add("movimento");
-              }
+          if (item.classList.contains(classe)) {
+            for (let i = 0; i < elemento.length; i++) {
+              elemento[i].classList.remove(classe);
+              elemento[i].classList.remove("movimento");
             }
-            if (tabuleiro.classList.contains("movimentoPreto")) {
-              if (item.classList.contains("preto")) {
-                item.classList.add(classe);
-                item.classList.add("movimento");
+          } else {
+            for (let i = 0; i < elemento.length; i++) {
+              elemento[i].classList.remove(classe);
+              elemento[i].classList.remove("movimento");
+            }
+            if (item.innerHTML !== "") {
+              const tabuleiro = document.querySelector(".tabuleiro");
+              if (tabuleiro.classList.contains("movimentoBranco")) {
+                if (item.classList.contains("branco")) {
+                  item.classList.add(classe);
+                  item.classList.add("movimento");
+                }
+              }
+
+              if (tabuleiro.classList.contains("movimentoPreto")) {
+                if (item.classList.contains("preto")) {
+                  item.classList.add(classe);
+                  item.classList.add("movimento");
+                }
               }
             }
           }
